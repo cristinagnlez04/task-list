501b0cf4fcdaa936fc898715f740cb1fde72d6a5
Cristina González <cristinagonzalez@MacBook-Air-de-Cristina-Gonzalez.local>
2019-11-29 17:27:25 -0300

    Add | Cambios en vista searchBar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { List } from "./list/List";
 import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <Container>
         <List />
       </Container>
