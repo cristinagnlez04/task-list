52b01d1e8d74b652ca4aa5f75c1729dd5c18ee6d
Cristina González <cristinagonzalez@MacBook-Air-de-Cristina-Gonzalez.local>
2019-11-29 16:50:21 -0300

    Add | Cambios en vista searchBar

--- src/list/List.js
+++ src/list/List.js
@@ -3,9 +3,13 @@ import Resultado from "./Resultado";
 import SearchBar from "./SearchBar";
 
 export function List(props) {
+  function onSearch(text) {
+    console.log("text", text);
+  }
+
   return (
     <>
-      <SearchBar />
+      <SearchBar onSearch={onSearch} />
       <Resultado />
     </>
   );
--- src/list/SearchBar.js
+++ src/list/SearchBar.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Form } from "react-bootstrap";
+import { func } from "prop-types";
 
-const SearchBar = () => <Form.Control placeholder="Buscar" />;
+const SearchBar = ({ onSearch }) => <Form.Control placeholder="Buscar" />;
+SearchBar.propTypes = {
+  onSearch: func.isRequired
+};
 
 export default SearchBar;
