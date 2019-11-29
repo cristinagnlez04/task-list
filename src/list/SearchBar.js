eb62ccdeee282ccb4f4d1e4550d2cf023821712e
Cristina González <cristinagonzalez@MacBook-Air-de-Cristina-Gonzalez.local>
2019-11-29 17:12:35 -0300

    Add | Cambios en vista searchBar

--- src/list/SearchBar.js
+++ src/list/SearchBar.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 import { func } from "prop-types";
+import { debounce } from "lodash";
 
-const SearchBar = ({ onSearch }) => <Form.Control placeholder="Buscar" />;
+const SearchBar = ({ onSearch }) => {
+  const debouncedSearch = debounce(onSearch, 1000);
+
+  return (
+    <Form.Control
+      placeholder="Buscar"
+      onChange={e => debouncedSearch(e.target.value)}
+    />
+  );
+};
+
 SearchBar.propTypes = {
   onSearch: func.isRequired
 };
